88d1ab517ab529aca0ba5e722ae902fb763d6754
brigitta9 <75415492+brigitta9@users.noreply.github.com>
2020-12-16 09:16:32 +0700

    Add files via upload

---
+++ data/TEA_1.js
@@ -0,0 +1,8 @@
+var json_TEA_1 = {
+"type": "FeatureCollection",
+"name": "TEA_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 106.821883341987544, -6.17151503200682 ] } }
+]
+}
